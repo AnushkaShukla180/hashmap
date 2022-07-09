bbfa0a9cfc3105635280328947b2c86cb6c08dea
ANUSHKA SHUKLA <82254904+AnushkaShukla180@users.noreply.github.com>
2022-07-09 10:21:21 -0700

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int a,b,sum,diff,mul,divi;
+cout<<"enter first number"<<endl;
+cin>>a;
+cout<<"enter second number"<<endl;
+cin>>b;
+cout<<"The sum is "<<a + b;
+cout<<"The diff is "<<a - b;
+cout<<"The mul is "<<a * b;
+cout<<"The divi is "<<(float) a / b;
+
+
+    return 0;
+}
