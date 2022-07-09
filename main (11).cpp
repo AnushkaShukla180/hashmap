0815e8b967e60c58c121aecfaf19c6c1a5b0d8bf
ANUSHKA SHUKLA <82254904+AnushkaShukla180@users.noreply.github.com>
2022-07-09 12:25:50 -0700

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+string name="harry";
+cout<<name<<endl;
+cout<<name.length()<<endl;
+cout<<name.size()<<endl;
+cout<<name.substr(2,3)<<endl;
+return 0;
+
+
+}
+
+
