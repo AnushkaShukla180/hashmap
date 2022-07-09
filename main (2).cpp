465a47f4490a28e29e010780effe69bf11cb9383
ANUSHKA SHUKLA <82254904+AnushkaShukla180@users.noreply.github.com>
2022-07-09 10:16:21 -0700

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int a,b,sum;
+cout<<"enter first number"<<endl;
+cin>>a;
+cout<<"enter second number"<<endl;
+cin>>b;
+cout<<"The sum is "<<a + b;
+
+
+    return 0;
+}
