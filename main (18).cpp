4cc9796691f37d9d6180d6d415a13a966df07362
ANUSHKA SHUKLA <82254904+AnushkaShukla180@users.noreply.github.com>
2022-07-10 12:37:48 -0700

    Add files via upload

---
+++ main (18).cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a=2,b=3,c=4,d=5,e=6,f=7,g=8,h=9,k=10;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<a*i<<b*i<<c*i<<d*i<<e*i<<f*i<<g*i<<h*i<<k*i<<endl;
+    }
+
+    return 0;
+}
